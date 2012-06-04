fe1ef1325a9bac8ef4e0ee95c9eeaa9f2c81a317
Earle Clubb <eclubb@valcom.com>
2012-06-04 17:07:37 -0400

    Add ability to mark a job as requiring approval.

--- lib/resque-approval.rb
+++ lib/resque-approval.rb
@@ -4,6 +4,31 @@ require 'resque'
 module Resque
   module Plugins
     module Approval
+      def before_enqueue_approval(*args)
+        args = args[0] || {}
+
+        requires_approval = args.delete(:requires_approval)
+        if requires_approval
+          enqueue_for_approval(*args)
+          allow_enqueue = false
+        else
+          allow_enqueue = true
+        end
+
+        allow_enqueue
+      end
+
+      def enqueue_for_approval(*args)
+        Resque.enqueue_to(:approval_required, self, *args)
+
+        id = Resque.size(:approval_required) - 1
+        key = { :id => id }.to_json
+
+        job = Resque.peek(:approval_required, id)
+        value = job.to_json
+
+        Resque.redis.hset('pending_jobs', key, value)
+      end
     end
   end
 end
--- spec/lib/resque-approval_spec.rb
+++ spec/lib/resque-approval_spec.rb
@@ -1,7 +1,59 @@
 require 'spec_helper'
 
+class Job
+  extend Resque::Plugins::Approval
+
+  @queue = 'dummy'
+
+  def self.perform
+  end
+end
+
 describe "Resque::Plugins::Approval" do
+  before do
+    Resque.remove_queue(:dummy)
+    Resque.remove_queue(:approval_required)
+    Resque.redis.del('pending_jobs')
+  end
+
   it "is a valid Resque plugin" do
     lambda { Resque::Plugin.lint(Resque::Plugins::Approval) }.should_not raise_error
+  end
+
+  describe ".before_enqueue_approval" do
+    context "when a job requires approval" do
+      it "calls enqueue_for_approval" do
+        Job.should_receive(:enqueue_for_approval).with()
+        Resque.enqueue(Job, :requires_approval => true)
+      end
+    end
+
+    context "when a job does not require approval" do
+      it "does not call enqueue_for_approval" do
+        Job.should_not_receive(:enqueue_for_approval)
+        Resque.enqueue(Job)
+      end
+    end
+  end
+
+  describe ".enqueue_for_approval" do
+    it "adds the job to the appoval queue" do
+      Job.enqueue_for_approval
+      Resque.size(:approval_required).should == 1
+    end
+
+    it "does not add the job to the dummy queue" do
+      Job.enqueue_for_approval
+      Resque.size(:dummy).should == 0
+    end
+
+    it "adds an entry to the 'pending_jobs' hash" do
+      Job.enqueue_for_approval()
+
+      key = '{"id":0}'
+      value = '{"class":"Job","args":[]}'
+
+      Resque.redis.hget('pending_jobs', key).should == value
+    end
   end
 end
